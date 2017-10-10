ae3426b08149dd060ff722d9a96abef67fa27ad7
deepfriedfilth <nkawatski@gmail.com>
2017-10-10 14:23:42 -0500

    Add examples of inline-block'd, float'd + grid'd grids

--- html/js/main.js
+++ html/js/main.js
@@ -9,10 +9,50 @@ jQuery(document).ready(function($) {
 		//$('#displayProp').html('grid');
 
 		var typed = new Typed('#displayProp', {
-			strings: ['display<span>:</span>', 'display<span>:</span> <strong>grid</strong><span>;</span>'],
-			typeSpeed: 100
+			strings: ['', 'display<span>:</span> <strong>grid</strong><span>;</span>'],
+			typeSpeed: 100,
+			smartBackspace: true
 		});
 	}, 300);
+
+	var waypoints = $('code').waypoint(function(dir) {
+        if(dir == 'down') {
+
+        	var el = $('#'+$(this)[0].element.id);
+        	var eld = '#'+el.find('div').attr('id');
+        	var finalString = $(eld).html();
+
+        	// if(!el.hasClass('html')) { // check if <code> has inner <div>
+        		console.log('data-typed='+$(eld).attr('data-typed'));
+	        	if($(eld).attr('data-typed') == 'false' && $(eld).attr('data-typing') == 'false') {// !$(eld).data('typed')
+	        		$(eld).html(''); //reset content
+	        		$(el).addClass('show');
+
+		        	var typeThis = new Typed(eld, {
+		        		strings: ['', finalString],
+		        		typeSpeed: 200,
+		        		onStart: function() {
+		        			$(eld).attr('data-typing',true);
+		        			console.log(eld+' has started being typed');
+		        		},
+		        		onComplete: function() {
+		        			console.log(eld+' has been typeded');
+		        			$(eld).attr('data-typing',false);
+		        			$(eld).attr('data-typed',true);
+		        			console.log('data-typed='+$(eld).attr('data-typed'));
+		        			//$(eld).data('typed',true);
+		        		}
+		        	});
+		        } else {
+		        	console.log('already typed '+eld);
+		        }
+	    	// } else {
+	    	// 	console.log('no inner divs, yo')
+	    	// }
+        }
+      }, {
+        offset: 200
+      });
 	
 
 	
